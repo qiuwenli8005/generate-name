9b67623f1ab8d65fba9c67fa8310420b893627f6
qiuwenli8005 <qiu_wenli@126.com>
2020-08-26 21:16:38 +0800

    启动类

--- src/main/java/com/qiu/name/GoNameApplication.java
+++ src/main/java/com/qiu/name/GoNameApplication.java
@@ -1,4 +1,12 @@
 package com.qiu.name;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class GoNameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoNameApplication.class);
+    }
 }
